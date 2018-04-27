a39c80a868cfb79d6b0556c489236a057f17ad7b
Tomas Patro <tomas.patro@gmail.com>
2018-04-27 22:30:04 +0200

    [Edit][main.py,.gitignore] Finally done parsing of a message and printing the send/recv messages. Added record to the .gitignore

--- src/main.py
+++ src/main.py
@@ -1,23 +1,110 @@
 #!/usr/bin/python3
 import socket
 import sys
+from binascii import hexlify
+
+BUFFER_SIZE = 512
+SERVER_ADDRESS = '127.0.0.1', 4000
+
+class DatagramControl():
+    '''Class which receives messages'''
+    def __init__(self, sock):
+        '''Cosntructor'''
+        self.sock = sock
+        self.address = None
+    
+    def getDatagram(self):
+        '''Method which will receive a new datagram'''
+        data = self.sock.recvfrom(BUFFER_SIZE)[0]
+        return data
+    
+    def receive(self):
+        '''Method which will parse a datagram and return tuple'''
+        data = self.getDatagram()
+        # (0,4)-ID, (4,6)-seq, (6,8)-ack, (8,9)-flags, (9,END)-data
+        return data[0:4], data[4:6], data[6:8], data[8:9], data[9:]
+
+    def send(self, message):
+        '''Method which will send a datagram'''
+        self.sock.sendto(message.encode(), SERVER_ADDRESS)
+    
+    def print(self, message, type):
+        '''Method which prints datagram'''
+        print(str(hexlify(message[0]), 'utf-8') + ' ' + type, '', end=' ')
+        tmp = int(str(hexlify(message[1]), 'utf-8'), base=16)
+        print('seq=' + str(tmp), '', end=' ')
+        tmp = int(str(hexlify(message[1]), 'utf-8'), base=16)
+        print('ack=' + str(tmp), '', end=' ')
+        print('flags=' + str(hexlify(message[3]), 'utf-8'), end=' ')
+        print('data(' + str(len(message[4])) + '):', end=' ')
+        if len(message[4]) > 1:
+            for i in range(0, 8):
+                print(hex(message[4][i])[2:], end=' ')
+            print('...', end=' ')
+            for i in range (len(message[4]) - 1, len(message[4]) - 9, -1):
+                print(hex(message[4][i])[2:], end=' ')
+        elif len(message[4]) == 1:
+            print(str(hexlify(message[4]), 'utf-8'), end='')
+        else:
+            print('-')
+        print('')
+
+class Handler():
+    '''Class which handles received messages'''
+    def __init__(self, sock):
+        '''Constructor'''
+        self.sock = sock
+        self.controler = DatagramControl(sock)
+
+    def handle(self, operation):
+        '''Method which handles required operation (0-download, 1-upload)'''
+        try:
+            if operation:
+                u = Upload(self.sock)
+                u.start()
+            else:
+                d = Download(self.sock)
+                d.start()
+        except KeyboardInterrupt:
+            print('Killed by KeyboardInterrupt')
+
+class Download():
+    '''Class which handles downloading of a picture from the probe'''
+    def __init__(self, sock):
+        '''Constructor'''
+        self.sock = sock
+        self.controler = DatagramControl(self.sock)
+        self.id = b''
+
+    def start(self):
+        '''Method which starts and handles downloading'''
+        self.setup()
+    
+    def setup(self):
+        '''Method which tells the probe that we are going to download a picture'''
+        sent = (0).to_bytes(8, 'big') + (4).to_bytes(1, 'big') + (1).to_bytes(1, 'big')
+        to_print = (0).to_bytes(4, 'big'), (0).to_bytes(2, 'big'), (0).to_bytes(2, 'big'), (4).to_bytes(1, 'big'), (1).to_bytes(1, 'big')
+        self.controler.print(to_print, 'SEND')
+        self.sock.sendto(sent, SERVER_ADDRESS)
+        received = self.controler.receive()
+        self.controler.print(received, 'RECV')
+        
+class Upload():
+    '''Class which handles uploading of a firmware to the probe'''
+    def __init__(self, sock):
+        '''Constructor'''
+        self.sock = sock
+        self.controler = DatagramControl(self.sock)
+    
+    def start(self):
+        '''Method which starts and handles uploading'''
 
 def main():
-    # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Bind the socket to the port
     server_address = ('localhost', 10000)
-    print('starting up on {} port {}'.format(*server_address))
+    print('Starting up on {}, port {}\n'.format(*server_address))
     sock.bind(server_address)
-
-    while True:
-        print('\nwaiting to receive message')
-        data, address = sock.recvfrom(4096)
-
-        print('Received: ', data.decode('ascii'))
-
-        if data:
-            sock.sendto('data received'.encode(), address)
-
+    h = Handler(sock)
+    h.handle(False)
+    
 main ()
